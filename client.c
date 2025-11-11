2068071aa902bf05bacd7127dbde8f576b5503aa
Adrian-rospx <adrianrosu5400@gmail.com>
2025-11-11 22:55:26 +0200

    fix: successful client to server interaction

--- client.c
+++ client.c
@@ -1,9 +1,9 @@
-#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 typedef struct sockaddr_in sockaddr_in;
@@ -20,6 +20,9 @@ int main () {
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_port);
 
+    // convert ip string to binary
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
     // connect to server
     if (connect(sock_client_fd, (sockaddr*)&server_addr, sizeof(server_addr))) {
         perror("Connect failed!");
@@ -27,13 +30,13 @@ int main () {
     }
 
     // send message
-    const char* message = "This is the client sent message!";
+    const char* message = "This is the message sent by the client!";
     send(sock_client_fd, message, strlen(message), 0);
 
     // recieve response
     char buffer[1024] = {0};
-    int bytes = read(sock_client_fd, buffer, strlen(buffer));
-    fprintf(stdout, "The server sent: %s\n", buffer);    
+    int bytes = read(sock_client_fd, buffer, sizeof(buffer));
+    fprintf(stdout, "The server sent back: %s\n", buffer);    
 
     close(sock_client_fd);
 
