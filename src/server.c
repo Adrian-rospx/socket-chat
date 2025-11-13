85b2ae8ffc30a9e3626086781cef359d7a50ea50
Adrian-rospx <adrianrosu5400@gmail.com>
2025-11-13 19:47:34 +0200

    added: async client connection with polling

--- src/server.c
+++ src/server.c
@@ -1,3 +1,4 @@
+#include <sys/poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,44 +9,91 @@
 
 #include "server.h"
 
-int handle_connection(const int socket_fd) {
-    // client address
-    sockaddr_in client_addr = {};
-    socklen_t client_len = sizeof(client_addr);
+typedef struct pollfd pollfd;
 
-    // accept connection
-    int client_fd = accept(socket_fd, (sockaddr*)&client_addr, &client_len);
-    if (client_fd == -1) {
-        perror("Accept failed!");
-        return -1;
-    }
-    fputs("Client connected!\n", stdout);
+// int handle_connection(const int socket_fd) {
+//     // client address
+//     sockaddr_in client_addr = {};
+//     socklen_t client_len = sizeof(client_addr);
 
-    // write to screen
-    char buffer[1024] = {0};
-    int bytes = read(client_fd, buffer, sizeof(buffer));
-    fprintf(stdout, "Recieved:\n%s\n", buffer);
+//     // accept connection
+//     int client_fd = accept(socket_fd, (sockaddr*)&client_addr, &client_len);
+//     if (client_fd == -1) {
+//         perror("Accept failed!");
+//         close(client_fd);
+//         return -1;
+//     }
+//     fputs("Client connected!\n", stdout);
 
-    // send message
-    const char* message = "Message recieved";
-    send(client_fd, message, strlen(message), 0);
-    close(client_fd);
+//     // write to screen
+//     char buffer[1024] = {0};
+//     int bytes = read(client_fd, buffer, sizeof(buffer));
+//     fprintf(stdout, "Recieved:\n%s\n", buffer);
 
-    return 0;
-}
+//     // send message
+//     const char* message = "Message recieved";
+//     send(client_fd, message, strlen(message), 0);
+//     close(client_fd);
+
+//     return 0;
+// }
 
 int run_server() {
     // initialisation
     int socket_fd;
     sockaddr_in server_addr;
-    if (start_server(&socket_fd, &server_addr) == -1) {
-        close(socket_fd);
+    if (start_server(&socket_fd, &server_addr) == -1)
         return -1;
+
+    // setup polling
+    pollfd fds[10] = {0};
+    pollfd server_poll = {};
+    server_poll.fd = socket_fd;
+    server_poll.events = POLLIN;
+    fds[0] = server_poll;
+    int poll_count = 1;
+
+    char buffer[256];
+
+    while (1) {
+        // poll indefinetely
+        int ret = poll(fds, 10, -1);
+        if (ret < 0) {
+            perror("Poll failed");
+            continue;
+        }
+
+        // loop through sockets
+        for (int i = 0; i < 10; i++) {
+            if (fds[i].fd == 0) continue;
+            // if it's the listening socket create a new connection
+            else if (fds[i].fd == socket_fd && (fds[i].revents & POLLIN)) {
+                sockaddr_in client_addr = {};
+                socklen_t client_len = sizeof(client_addr);
+
+                int client_fd = accept(socket_fd, (sockaddr*)&client_addr, &client_len);
+                if (client_fd < 0) {
+                    perror("Accept failed!");
+                    continue;
+                }
+
+                pollfd client_poll = {};
+                client_poll.fd = client_fd;
+                client_poll.events = POLLIN;
+
+                fds[poll_count] = client_poll;
+                poll_count++;
+
+                fprintf(stdout, "New client connected with fd = %d\n", client_fd);
+            }
+        }
+
     }
 
     // client connection config
-    handle_connection(socket_fd);
-    handle_connection(socket_fd);
+    // while (1) {
+    //     if (handle_connection(socket_fd) == -1) return -1;
+    // }
 
     close(socket_fd);
 
--- src/server_start.c
+++ src/server_start.c
@@ -2,18 +2,33 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "server.h"
 
 const int port = 8765;
-const int max_connections = 10;
+const int max_queued_connections = 10;
 
+// create a non-blocking socket
 int init_socket() {
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd == -1) {
         perror("Socket failed!");
+        close(socket_fd);
         return -1;
     }
+
+    int flags = fcntl(socket_fd, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL");
+        return -1;
+    }
+    if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl F_SETFL");
+        return -1;
+    }
+
     return socket_fd;
 }
 
@@ -31,14 +46,16 @@ sockaddr_in socket_address_setup() {
 int bind_socket(const int socket_fd, sockaddr_in* address_p) {
     if (bind(socket_fd, (sockaddr*)address_p, sizeof(*address_p)) < 0) {
         perror("Bind failed!");
+        close(socket_fd);
         return -1;
     }
     return 0;
 }
 
 int start_listener(const int socket_fd) {
-    if (listen(socket_fd, max_connections) < 0) {
+    if (listen(socket_fd, max_queued_connections) < 0) {
         perror("Listen failed!");
+        close(socket_fd);
         return -1;
     }
     fprintf(stdout, "Listening on port %hd\n", port);
